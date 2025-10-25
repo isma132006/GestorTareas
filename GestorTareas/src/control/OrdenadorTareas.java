568e87378f96e3ab5d454686f45cb168f2c1432a
Ismael Soto <isma132006@ciencias.unam.mx>
2025-10-24 21:14:15 -0600

    Simplify sorting method by removing comments

--- GestorTareas/src/control/OrdenadorTareas.java
+++ GestorTareas/src/control/OrdenadorTareas.java
@@ -9,8 +9,6 @@ public class OrdenadorTareas {
     // Método genérico para ordenar por prioridad
     public static <T extends Tarea> List<T> ordenarPorPrioridad(List<T> tareas) {
         List<T> copia = new ArrayList<>(tareas);
-        // Puedes usar quickSort o Collections.sort con Comparator
-        // Por simplicidad, aquí te pongo Collections.sort con Comparator inverso (prioridad descendente)
         copia.sort((t1, t2) -> Integer.compare(t2.getPrioridad(), t1.getPrioridad()));
         return copia;
     }
@@ -21,6 +19,4 @@ public class OrdenadorTareas {
         copia.sort((t1, t2) -> t1.getFechaLimite().compareTo(t2.getFechaLimite()));
         return copia;
     }
-
-    // Si quieres puedes agregar quickSort genérico también, pero con sort es suficiente
 }
